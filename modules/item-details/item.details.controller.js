0bbdf1bff74ce2a6df9434a1a1d26ea05aac2532
Ashish Dwivedi <ashishdwivedi1909@yahoo.in>
2017-02-12 09:07:48 +0530

    Price updation

--- modules/item-details/item.details.controller.js
+++ modules/item-details/item.details.controller.js
@@ -69,7 +69,7 @@
 		
 		function removeFromCart() {
 			_this.detailsData.isInCart = false;
-			_this.cartData.totalPrice = _this.cartData.totalPrice - thisFruit.price;
+			_this.cartData.totalPrice = _this.cartData.totalPrice - (thisFruit.price*thisFruit.quantity);
 			for(var i=0; _this.cartData.items.length; i++) {
 				if(_this.cartData.items[i].id === thisFruit.id) {
 					_this.cartData.items.splice(i, 1);
